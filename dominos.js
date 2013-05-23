788de8e13c7be65da7727798ace5a9f2cc581c24
abw333 <alanwagner333@gmail.com>
2013-05-23 13:47:10 -0400

    both sides curve upwards when they get too close to the edge

--- dominos.js
+++ dominos.js
@@ -46,49 +46,100 @@ $(function() {
 
       var domino = game.chain[game.chain_center];
       domino.image.style.position = "absolute";
-      domino.image.style.top = "250px";
+      domino.image.style.top = "450px";
    	  domino.image.style.left = "660px";
       document.body.appendChild(domino.image);
 
       var position = 691;
+      var stage = 1;
+      var last_position;
       for (var domino_i = game.chain_center + 1; domino_i < game.chain.length; domino_i++) {
       	var domino = game.chain[domino_i];
         domino.image.style.position = "absolute";
-      	domino.image.style.left = position.toString() + "px";
-      	if (domino.first == domino.second) {
-          domino.setOrientation("b");
-          domino.image.style.top = "250px";
-          position += 31;
-      	} else {
-          if (game.directions[domino_i] == "in") {
-            domino.setOrientation("l");
-          } else {
-            domino.setOrientation("r");
+        if (stage == 1) {
+        	domino.image.style.left = position.toString() + "px";
+        	if (domino.first == domino.second) {
+            domino.setOrientation("b");
+            domino.image.style.top = "450px";
+            position += 31;
+        	} else {
+            if (game.directions[domino_i] == "in") {
+              domino.setOrientation("l");
+            } else {
+              domino.setOrientation("r");
+            }
+            domino.image.style.top = "465px";
+            position += 61;
+        	}
+          var is_double = domino.first == domino.second;
+          var next_is_double = domino_i + 1 < game.chain.length && game.chain[domino_i + 1].first == game.chain[domino_i + 1].second;
+          if (position > 1000 && !is_double && !next_is_double) {
+            last_position = position - 31;
+            stage = 2;
+            position = 404;
           }
-          domino.image.style.top = "265px";
-          position += 61;
-      	}
+        } else if (stage == 2) {
+          if (domino.first == domino.second) {
+            domino.image.style.left = (last_position - 15).toString() + "px";
+            domino.setOrientation("r");
+            position -= 31;
+          } else {
+            domino.image.style.left = (last_position).toString() + "px";
+            if (game.directions[domino_i] == "in") {
+              domino.setOrientation("b");
+            } else {
+              domino.setOrientation("t");
+            }
+            position -= 61;
+          }
+          domino.image.style.top = (position + 61).toString() + "px";
+        }
         document.body.appendChild(domino.image);
       }
 
       position = 660;
+      stage = 1;
       for (var domino_i = game.chain_center - 1; domino_i >= 0; domino_i--) {
       	var domino = game.chain[domino_i];
-      	if (domino.first == domino.second) {
-          domino.setOrientation("b");
-          domino.image.style.top = "250px";
-          position -= 31;
-      	} else {
-          domino.image.style.top = "265px";
-          position -= 61;
-          if (game.directions[domino_i] == "in") {
-            domino.setOrientation("r");
-          } else {
-            domino.setOrientation("l");
-          }
-      	}
-      	domino.image.style.left = position.toString() + "px";
         domino.image.style.position = "absolute";
+        if (stage == 1) {
+        	if (domino.first == domino.second) {
+            domino.setOrientation("b");
+            domino.image.style.top = "450px";
+            position -= 31;
+        	} else {
+            domino.image.style.top = "465px";
+            position -= 61;
+            if (game.directions[domino_i] == "in") {
+              domino.setOrientation("r");
+            } else {
+              domino.setOrientation("l");
+            }
+        	}
+        	domino.image.style.left = position.toString() + "px";
+          var is_double = domino.first == domino.second;
+          var next_is_double = domino_i - 1 >= 0 && game.chain[domino_i - 1].first == game.chain[domino_i - 1].second;
+          if (position < 400 && !is_double && !next_is_double) {
+            last_position = position;
+            stage = 2;
+            position = 404;
+          }
+        } else if (stage == 2) {
+          if (domino.first == domino.second) {
+            domino.image.style.left = (last_position - 15).toString() + "px";
+            domino.setOrientation("r");
+            position -= 31;
+          } else {
+            domino.image.style.left = (last_position).toString() + "px";
+            if (game.directions[domino_i] == "in") {
+              domino.setOrientation("b");
+            } else {
+              domino.setOrientation("t");
+            }
+            position -= 61;
+          }
+          domino.image.style.top = (position + 61).toString() + "px";
+        }
         document.body.appendChild(domino.image);
       }
 	}
@@ -101,13 +152,9 @@ $(function() {
       } else {
       	move = moves[Math.floor(Math.random() * moves.length)];
       }
-      console.log(moves);
-      console.log(move);
       game.makeMove(move.domino_i, move.side);
       drawGame(ctx, game);
-      game.print();
 	});
 
 	drawGame(ctx, game);
-	game.print();
 });
