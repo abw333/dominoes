f62f108e6f5895808765b2a762c3241397818956
Alan Wagner <alanwagner333@gmail.com>
2016-01-02 19:13:20 -0400

    changed print statements to print function calls for python3 compatibility

--- domino/play_random_game.py
+++ domino/play_random_game.py
@@ -2,14 +2,14 @@ import domino
 import random
 
 game = domino.Game(starting_domino=domino.Domino(6, 6))
-print game
+print(game)
 
 while True:
     valid_moves = game.valid_moves()
     random_move = random.choice(valid_moves)
     result = game.make_move(*random_move)
 
-    print game
+    print(game)
 
     if result is not None:
         break
