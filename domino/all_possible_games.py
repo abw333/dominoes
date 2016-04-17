764317f970645f0cd49d944f240fa6bf2dc4210f
Alan Wagner <alanwagner333@gmail.com>
2016-04-16 22:10:11 -0400

    timing calculation of all possible games

--- domino/all_possible_games.py
+++ domino/all_possible_games.py
@@ -1,18 +1,20 @@
+import common
 import copy
 import domino
 
-in_progress = [domino.Game()]
-completed = []
+with common.stopwatch('Computing all possible games'):
+    in_progress = [domino.Game()]
+    completed = []
 
-while in_progress:
-    game = in_progress.pop()
-    moves = game.valid_moves()
-    for move in moves:
-        new_game = copy.deepcopy(game)
-        result = new_game.make_move(*move)
-        if result is None:
-            in_progress.append(new_game)
-        else:
-            completed.append(new_game)
+    while in_progress:
+        game = in_progress.pop()
+        moves = game.valid_moves()
+        for move in moves:
+            new_game = copy.deepcopy(game)
+            result = new_game.make_move(*move)
+            if result is None:
+                in_progress.append(new_game)
+            else:
+                completed.append(new_game)
 
-print(len(completed))
+    print(len(completed))
