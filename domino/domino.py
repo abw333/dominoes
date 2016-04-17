73b3b5d5dbad82f102d2268ade3b7f258e654429
Alan Wagner <alanwagner333@gmail.com>
2016-04-17 17:52:03 -0400

    hash function for Domino class

--- domino/domino.py
+++ domino/domino.py
@@ -13,11 +13,13 @@ class Domino:
         return '[{0}|{1}]'.format(self.first, self.second)
 
     def __eq__(self, other):
-        return (self.first, self.second) == (other.first, other.second) \
-            or (self.first, self.second) == (other.second, other.first)
+        return sorted((self.first, self.second)) == sorted((other.first, other.second))
 
     def __ne__(self, other):
         return not self == other
+
+    def __hash__(self):
+        return hash(tuple(sorted((self.first, self.second))))
 
     def __contains__(self, key):
         return key == self.first or key == self.second
