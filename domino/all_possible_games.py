d2d13ac13f4ec57e342f8ce4457bd1a4355fe961
Alan Wagner <alanwagner333@gmail.com>
2016-04-16 22:30:46 -0400

    not double counting moves when the same number is at both ends of the board; factoring out search function in preparation for multithreading

--- domino/all_possible_games.py
+++ domino/all_possible_games.py
@@ -5,20 +5,17 @@ import random
 
 FIXED_MOVES = 10
 
-with common.stopwatch('Computing all possible games'):
-    game = domino.Game()
-
-    for i in range(FIXED_MOVES):
-        moves = game.valid_moves()
-        move = random.choice(moves)
-        game.make_move(*move)
-
+def compute_all_possible_games(game):
     in_progress = [game]
     completed = []
 
     while in_progress:
         game = in_progress.pop()
         moves = game.valid_moves()
+
+        if game.board.left_end() == game.board.right_end():
+            moves = [m for m in moves if m[1] == 'LEFT']
+
         for move in moves:
             new_game = copy.deepcopy(game)
             result = new_game.make_move(*move)
@@ -27,4 +24,15 @@ with common.stopwatch('Computing all possible games'):
             else:
                 completed.append(new_game)
 
+    return completed
+
+with common.stopwatch('Computing all possible games'):
+    game = domino.Game()
+
+    for i in range(FIXED_MOVES):
+        moves = game.valid_moves()
+        move = random.choice(moves)
+        game.make_move(*move)
+
+    completed = compute_all_possible_games(game)
     print(len(completed))
