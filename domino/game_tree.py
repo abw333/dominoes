e77b078bbf54f48c388b82415da42fa86656382c
Alan Wagner <alanwagner333@gmail.com>
2016-04-17 20:35:17 -0400

    rolled bfs_step() into bfs(), since it is no longer needed on its own

--- domino/game_tree.py
+++ domino/game_tree.py
@@ -2,28 +2,6 @@ import copy
 import numpy
 import os
 
-def bfs_step(nodes):
-    new_nodes = []
-
-    def make_move(node, game, move):
-        node.children[move] = Node(result=game.make_move(*move),
-                                   parent_node=node, parent_move=move)
-        if node.children[move].result is None:
-            node.children[move].game = game
-            new_nodes.append(node.children[move])
-
-    for node in nodes:
-        moves = node.game.valid_moves()
-        for move in moves[:-1]:
-            new_game = copy.deepcopy(node.game)
-            make_move(node, new_game, move)
-
-        make_move(node, node.game, moves[-1])
-
-        node.game = None
-
-    return new_nodes
-
 class Node:
     def __init__(self, game=None, children=None, result=None,
                  parent_node=None, parent_move=None):
@@ -48,8 +26,27 @@ class Node:
         depth = 0
 
         while nodes and depth < max_depth:
-            nodes = bfs_step(nodes)
+            new_nodes = []
 
+            def make_move(node, game, move):
+                node.children[move] = Node(result=game.make_move(*move),
+                                           parent_node=node, parent_move=move)
+                if node.children[move].result is None:
+                    node.children[move].game = game
+                    new_nodes.append(node.children[move])
+
+            for node in nodes:
+                moves = node.game.valid_moves()
+                for move in moves[:-1]:
+                    new_game = copy.deepcopy(node.game)
+                    make_move(node, new_game, move)
+
+                make_move(node, node.game, moves[-1])
+
+                node.game = None
+
+            nodes = new_nodes
             depth += 1
+
             print('Process {}, Depth {}: {} active games'.format(
                     pid, depth, len(nodes)))
