7bed7ba316767464fef053f49b16f9f0b570d335
Alan Wagner <alanwagner333@gmail.com>
2016-10-24 22:34:57 -0400

    not using a utilities file

--- all_possible_games.py
+++ all_possible_games.py
@@ -1,13 +1,22 @@
-import common
+import contextlib
 import copy
 import domino
 import multiprocessing
 import random
+import time
 
 FIXED_MOVES = 30
 SERIAL_DEPTH = 5
 NUM_PROCESSES = 12
 CHUNK_SIZE = 1
+
+@contextlib.contextmanager
+def stopwatch(message):
+    print(message)
+    start = time.time()
+    yield
+    elapsed = time.time() - start
+    print(message, 'took {:.2f} seconds'.format(elapsed))
 
 def run_bfs(node):
     node.bfs()
@@ -16,10 +25,10 @@ def run_bfs(node):
 
 def all_possible_games(fixed_moves=FIXED_MOVES, serial_depth=SERIAL_DEPTH,
                        num_processes=NUM_PROCESSES):
-    with common.stopwatch('Initializing random game'):
+    with stopwatch('Initializing random game'):
         game = domino.Game()
 
-    with common.stopwatch('Playing {} moves at random'.format(fixed_moves)):
+    with stopwatch('Playing {} moves at random'.format(fixed_moves)):
         for i in range(fixed_moves):
             moves = game.valid_moves()
             move = random.choice(moves)
@@ -29,20 +38,20 @@ def all_possible_games(fixed_moves=FIXED_MOVES, serial_depth=SERIAL_DEPTH,
                 print('Fixed moves ended the game - returning early.')
                 return
 
-    with common.stopwatch('Saving original game state'):
+    with stopwatch('Saving original game state'):
         orig_game = copy.deepcopy(game)
 
-    with common.stopwatch('Changing to skinny board representation'):
+    with stopwatch('Changing to skinny board representation'):
         game.skinny_board()
 
-    with common.stopwatch('Initializing game tree'):
+    with stopwatch('Initializing game tree'):
         root = domino.GameNode(game=game)
 
-    with common.stopwatch('Running BFS to depth {} serially'.format(serial_depth)):
+    with stopwatch('Running BFS to depth {} serially'.format(serial_depth)):
         root.bfs(max_depth=serial_depth, parent_pointers=True)
         nodes = list(root.leaf_nodes())
 
-    with common.stopwatch('Running remaining BFS using {} processes'.format(num_processes)):
+    with stopwatch('Running remaining BFS using {} processes'.format(num_processes)):
         with multiprocessing.Pool(num_processes) as pool:
             searched_nodes = []
             for i, searched_node in enumerate(pool.imap(run_bfs, nodes, CHUNK_SIZE)):
@@ -52,14 +61,14 @@ def all_possible_games(fixed_moves=FIXED_MOVES, serial_depth=SERIAL_DEPTH,
                 pct_complete = round(100 * complete / len(nodes), 2)
                 print('{}/{} ({}%) tasks completed'.format(complete, len(nodes), pct_complete))
 
-    with common.stopwatch('Combining BFS results'):
+    with stopwatch('Combining BFS results'):
         for i, node in enumerate(nodes):
             node.parent_node.children[node.parent_move] = searched_nodes[i]
 
-    with common.stopwatch('Computing optimal play'):
+    with stopwatch('Computing optimal play'):
         moves, result = root.optimal_play()
 
-    with common.stopwatch('Printing optimal play'):
+    with stopwatch('Printing optimal play'):
         for move in moves:
             orig_game.make_move(*move)
             print(orig_game)
