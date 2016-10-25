ec1021e189a6422ff759ba24ae8eff1c1bb1f919
Alan Wagner <alanwagner333@gmail.com>
2016-10-24 22:29:26 -0400

    moving GameNode into the package

--- domino/__init__.py
+++ domino/__init__.py
@@ -1,5 +1,6 @@
 from domino.board import Board
 from domino.domino import Domino
 from domino.game import Game
+from domino.game_node import GameNode
 from domino.series import Series
 from domino.skinny_board import SkinnyBoard
