eb9a36f58f31a7b0a2c5755cdd9f7637494f676d
Alan Wagner <alanwagner333@gmail.com>
2016-10-27 23:55:36 -0400

    adding tests for inverted(), str(), and repr()

--- package/tests/test_domino.py
+++ package/tests/test_domino.py
@@ -8,5 +8,22 @@ class TestDomino(unittest.TestCase):
         self.assertEqual(d.first, 1)
         self.assertEqual(d.second, 2)
 
+    def test_inverted(self):
+        d = domino.Domino(1, 2)
+        d_inv = d.inverted()
+
+        self.assertEqual(d.first, d_inv.second)
+        self.assertEqual(d.second, d_inv.first)
+
+    def test_str(self):
+        d = domino.Domino(1, 2)
+
+        self.assertEqual(str(d), '[1|2]')
+
+    def	test_repr(self):
+        d = domino.Domino(1, 2)
+
+        self.assertEqual(repr(d), '[1|2]')
+
 if __name__ == '__main__':
     unittest.main()
