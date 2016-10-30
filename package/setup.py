c7644225a205c0eff43191b4a8a2598b3295aeba
Alan Wagner <alanwagner333@gmail.com>
2016-10-29 21:50:56 -0400

    update to setup.py

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 setup(
     name='domino',
-    version='1.0.0',
+    version='0.0.0',
+    description='Python implementation of the dominoes game.',
+    url='https://github.com/abw333/domino',
+    author='Alan Wagner',
+    author_email='alanwagner333@gmail.com',
     packages=['domino'],
     install_requires=['numpy>=1.11.2'],
     test_suite='tests'
