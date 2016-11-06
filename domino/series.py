2b6f9095cf3a59b48df98d0021595002dae63a83
Alan Wagner <alanwagner333@gmail.com>
2016-11-06 16:51:06 -0500

    Series docstring

--- domino/series.py
+++ domino/series.py
@@ -1,6 +1,33 @@
 import domino
 
 class Series:
+    '''
+    Python class for objects that represent a series of dominoes games.
+
+    A series of dominoes games is played with 4 players, split into two teams.
+    They will then play a sequence of games and keep a running tally of how
+    many points each team has scored. When a team's cumulative score surpasses
+    some predetermined threshold (usually 100 or 200), that team wins.
+
+    Prior to starting the series, the teams agree to a starting domino (usually
+    [6|6]). The player that draws this domino during the first game will play
+    first. The starting player for subsequent games is determined as follows:
+        * If a player wins by playing their last domino, that player will start
+          the following game.
+        * If a player makes the game stuck, and his/her team either wins or ties,
+          that player will start the following game.
+        * If a player makes the game stuck, and his/her team loses, then the
+          following player (from the other team) will start the following game.
+
+    :param int target_score: score up to which the series will be played;
+                             defaults to 200
+    :param Domino starting_domino: domino that will determine which player
+                                   starts the first game; defaults to [6|6]
+    :var games: ordered list of games played in the series
+    :var scores: list containing the two teams' scores; team 0 has players 0
+                 and 2, and team 1 has players 1 and 3
+    :var target_score: score up to which the series will be played
+    '''
     def __init__(self, target_score=200, starting_domino=None):
         if starting_domino is None:
             starting_domino = domino.Domino(6, 6)
