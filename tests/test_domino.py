2364499a40e7324c17144fa3b629f65761889471
Alan Wagner <alanwagner333@gmail.com>
2016-11-13 22:41:37 -0500

    updating test_domino.py with new package name

--- tests/test_domino.py
+++ tests/test_domino.py
@@ -1,42 +1,42 @@
 import collections
-import domino
+import dominoes
 import unittest
 
 class TestDomino(unittest.TestCase):
     def test_init(self):
-        d = domino.Domino(1, 2)
+        d = dominoes.Domino(1, 2)
 
         self.assertEqual(d.first, 1)
         self.assertEqual(d.second, 2)
 
     def test_inverted(self):
-        d = domino.Domino(1, 2)
+        d = dominoes.Domino(1, 2)
         d_inv = d.inverted()
 
         self.assertEqual(d.first, d_inv.second)
         self.assertEqual(d.second, d_inv.first)
 
     def test_str(self):
-        d = domino.Domino(1, 2)
+        d = dominoes.Domino(1, 2)
 
         self.assertEqual(str(d), '[1|2]')
 
     def test_repr(self):
-        d = domino.Domino(1, 2)
+        d = dominoes.Domino(1, 2)
 
         self.assertEqual(repr(d), '[1|2]')
 
     def test_eq(self):
-        d1 = domino.Domino(1, 2)
-        d2 = domino.Domino(1, 2)
-        d3 = domino.Domino(2, 1)
+        d1 = dominoes.Domino(1, 2)
+        d2 = dominoes.Domino(1, 2)
+        d3 = dominoes.Domino(2, 1)
 
         # order of values does not matter
         self.assertEqual(d1, d2)
         self.assertEqual(d1, d3)
 
-        d4 = domino.Domino(1, 1)
-        d5 = domino.Domino(1, 1)
+        d4 = dominoes.Domino(1, 1)
+        d5 = dominoes.Domino(1, 1)
 
         # it's OK if both values are the same
         self.assertEqual(d4, d5)
@@ -52,9 +52,9 @@ class TestDomino(unittest.TestCase):
         self.assertNotEqual(d1, pd)
 
     def test_hash(self):
-        d1 = domino.Domino(1, 2)
-        d2 = domino.Domino(2, 1)
-        d3 = domino.Domino(1, 3)
+        d1 = dominoes.Domino(1, 2)
+        d2 = dominoes.Domino(2, 1)
+        d3 = dominoes.Domino(1, 3)
 
         d_set = {d1}
 
@@ -63,7 +63,7 @@ class TestDomino(unittest.TestCase):
         self.assertFalse(d3 in d_set)
 
     def test_contains(self):
-        d = domino.Domino(1, 2)
+        d = dominoes.Domino(1, 2)
 
         self.assertTrue(1 in d)
         self.assertTrue(2 in d)
