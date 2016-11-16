0edd47a68b82395fe0b8c617a171b95b17ea7d41
Alan Wagner <alanwagner333@gmail.com>
2016-11-15 19:50:41 -0500

    minor modification to package description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dominoes',
     version='0.0.0',
-    description='A Python library for the dominoes game.',
+    description='A Python library for the game of dominoes.',
     url='https://github.com/abw333/dominoes',
     author='Alan Wagner',
     author_email='alanwagner333@gmail.com',
