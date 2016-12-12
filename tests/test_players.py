c88b185a8c10bb83d205c55df26469ea467ce97b
Alan Wagner <alanwagner333@gmail.com>
2016-12-11 20:55:09 -0500

    testing player interface for random

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,11 +1,25 @@
+import copy
 import dominoes
 import unittest
 
 class TestPlayers(unittest.TestCase):
-    def test_random(self):
+    def _test_player_interface(self, player):
         g = dominoes.Game.new()
 
-        dominoes.players.random(g)
+        g_copy = copy.deepcopy(g)
+
+        player(g)
+
+        self.assertEqual(type(g.valid_moves), tuple)
+        self.assertEqual(len(g.valid_moves), len(g_copy.valid_moves))
+        self.assertEqual(set(g.valid_moves), set(g_copy.valid_moves))
+
+        g_copy.valid_moves = g.valid_moves
+
+        self.assertEqual(g, g_copy)
+
+    def test_random(self):
+        self._test_player_interface(dominoes.players.random)
 
 if __name__ == '__main__':
     unittest.main()
