692105927b1741cb950b9f5c2de73e71f5348693
Alan Wagner <alanwagner333@gmail.com>
2016-12-28 14:29:50 -0400

    testing more intricate make_moves behavior

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,11 +1,45 @@
+import copy
 import dominoes
 import unittest
 
 class TestSearch(unittest.TestCase):
     def test_make_moves(self):
-        game = dominoes.Game.new()
+        game1 = dominoes.Game.new()
 
-        for vm, (m, g) in zip(game.valid_moves, dominoes.search.make_moves(game)):
+        for vm, (m, g) in zip(game1.valid_moves, dominoes.search.make_moves(game1)):
+            self.assertEqual(vm, m)
+
+            self.assertEqual(g.board.left_end(), m[0].first)
+            self.assertEqual(g.board.right_end(), m[0].second)
+            self.assertEqual(len(g.board), 1)
+            self.assertEqual(len(g.hands[0]), 6)
+            self.assertEqual(len(g.hands[1]), 7)
+            self.assertEqual(len(g.hands[2]), 7)
+            self.assertEqual(len(g.hands[3]), 7)
+            self.assertEqual(g.moves[0], m)
+            for p in g.moves[1:]:
+                self.assertIsNone(p)
+            self.assertIsNotNone(g.turn)
+            self.assertTrue(bool(g.valid_moves))
+            self.assertEqual(g.starting_player, 0)
+            self.assertIsNone(g.result)
+
+        game2 = dominoes.Game.new()
+        game2.result = True
+
+        self.assertEqual(list(dominoes.search.make_moves(game2)), [])
+
+        game3 = dominoes.Game.new()
+        game3_copy = copy.deepcopy(game3)
+
+        # there is a small chance that the valid moves are already
+        # sorted in bota gorda order, in which case this won't
+        # test anything interesting. this test suite gets run
+        # often enough that the danger is negligible.
+        dominoes.players.bota_gorda(game3_copy)
+
+        for vm, (m, g) in zip(game3_copy.valid_moves,
+                              dominoes.search.make_moves(game3, dominoes.players.bota_gorda)):
             self.assertEqual(vm, m)
 
             self.assertEqual(g.board.left_end(), m[0].first)
